c1a58fae0ce7d4b0e0df11cccd36ea6a56ceb9b1
Stefano Diaz <stefanodiaz1381@gmail.com>
2026-09-09 22:04:22 -0300

    Update sala2.py

--- salas/sala2.py
+++ salas/sala2.py
@@ -3,20 +3,12 @@ from funciones.salas.fn_sala2 import *
 
 
 
-def juego_batalla_naval():
-    # Generar tablero interno
+def jugar_batalla():
     matriz = crear_matriz()
     barcos = generar_barcos(matriz)
     while barcos == -1:
         chequeo = crear_matriz()
         barcos = generar_barcos(chequeo)
-
-    mapa = crear_mapa()
-    
-    # DATOS DEL JUGADOR
-    disparos = 5
-    hundidos = 0
-    restantes = 3
 
     print("""
 ░░░░░░░░  ░░░░░░░   ░░░░░░░░░   ░░░░░░░   ░░        ░░        ░░░░░░░ 
@@ -39,46 +31,20 @@ def juego_batalla_naval():
 ╰┴╯╵ ╵╰─╴╵ ╵╵╵ ╵╰─╯ ╵ ╰─╯╵ ╵   ╶┴╯╰─╴   ╵╵ ╵╰─╴.                                                        
     """) #logo del juego
 
-    # Logica del juego - Si el jugador se queda sin disparos, sale del bucle
-    while disparos != 0:
-
-        mostrar_mapa(mapa)
-        
-        # Ingreso y validacion de coordenadas
-        eleccion_1, eleccion_2 = eleccion()
-        
-        if barcos[eleccion_1-1][eleccion_2-1] == 2:
-            print("Esta posición ya fue intentada anteriormente, elija otra.")
-            eleccion_1, eleccion_2 = eleccion()
-
-        # Actualiza el mapa que muestra al jugador la posición atacada.
-        if barcos[eleccion_1-1][eleccion_2-1] == 1:
-            print("\n¡IMPACTO! Barco hundido")
-            mapa[eleccion_1-1][eleccion_2-1] = "X"
-            hundidos += 1
-            restantes -= 1
+    print("Su objetivo es derribar a los 3 barcos escondidos en este mapa de 5x5 elementos")
+    print("Las A marcaran sus fallos, donde hay agua, y las X marcaran sus aciertos")
+    
+    mapa = crear_mapa()
+    resultado = actualizar(mapa, barcos)
+    
+    if resultado == -1:
+        print("\n--PERDIÓ--")
+        opcion = gameover()
+        if opcion == 1:
+            return jugar_batalla() # Vuelve a ejecutar la función si elige reintentar
         else:
-            print("\nAGUA")
-            mapa[eleccion_1-1][eleccion_2-1] = "A"
+            return False # Retorna la sala como incompleta si decide volver al menú
+            
+    return True # Retorna la sala como completada
 
-        barcos[eleccion_1-1][eleccion_2-1] = 2
-        disparos -= 1
 
-        print("===================================")
-        print("Disparos restantes:", disparos)
-        print("Barcos hundidos:", hundidos)
-        print("Barcos restantes:", restantes)
-        print("===================================")
-
-        # Verifica si el jugador adivinó todos los barcos
-        if restantes == 0:
-            print("\n--FELICIDADES--")
-            print("¡Hundió todos los barcos!")
-            return True # Retorna la sala de escape como completada
-
-    # Si el jugador se queda sin disparos, le preguntamos qué quiere hacer
-    opcion = gameover()
-    if opcion == 1: # Hacemos que la función se llame a sí misma para que lo vuelva a intentar
-        juego_batalla_naval()
-    else:
-        return False # Retorna la sala de escape como incompleta
