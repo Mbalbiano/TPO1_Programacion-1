e27af56677bb2dff65eb355e00fc6ad53026495c
Morris01010101010101010101 <idamiano@uade.edu.ar>
2026-09-09 22:46:36 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,1 @@
-from gestion_usuarios import Menu
-
-menu() 
-#dada la estructura de carpetas, necesito llamar a la funcion desde el main para que no crashee, por eso existe esta linea, para probar el codigo :p
+from gestion_usuarios import Login
