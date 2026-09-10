65fbebfd6b18d6e0b5b469023b92aa879c3cc26d
Morris01010101010101010101 <idamiano@uade.edu.ar>
2026-09-09 22:10:31 -0300

    Update fn_sala2.py

--- funciones/salas/fn_sala2.py
+++ funciones/salas/fn_sala2.py
@@ -80,18 +80,3 @@ def generar_barcos(matriz):
     except:
         return -1
     return matriz
-
-def jugar_batalla():
-    matriz = crear_matriz()
-    barcos = generar_barcos(matriz)
-    while barcos ==-1:
-        chequeo = crear_matriz()
-        barcos = generar_barcos(chequeo)
-    print("Su objetivo es derribar a los 3 barcos escondidos en este mapa de 5x5 elementos")
-    print("Las A marcaran sus fallos, donde hay agua, y las X marcaran sus aciertos")
-    mapa = crear_mapa()
-    resultado = actualizar(mapa,barcos)
-    if resultado ==-1:
-        print("--PERDIO--") #Decidir que pasa si el jugador pierde
-        return False
-    return True
