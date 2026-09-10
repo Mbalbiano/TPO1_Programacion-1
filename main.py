d6f47cfbe398fb231922413819a5b12f9ca2710b
Stefano Diaz <stefanodiaz1381@gmail.com>
2026-09-09 22:24:53 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from salas import sala1
+from gestion_usuarios import Menu
 
-sala1.juego_ahorcado() 
+menu() 
 #dada la estructura de carpetas, necesito llamar a la funcion desde el main para que no crashee, por eso existe esta linea, para probar el codigo :p
