3d9809ff84c60aa1b6eb7a8ac20c7ac57d44781e
Morris01010101010101010101 <idamiano@uade.edu.ar>
2026-09-09 21:59:56 -0300

    Login a el sistema

---
+++ gestion_usuarios/Login.py
@@ -0,0 +1,13 @@
+from contra import validar_contraseña
+
+contraseña = "Unodos34%"
+usuario = "Jugador"
+U_usuario = input("Ingrese el nombre del jugador: ")
+while U_usuario != usuario:
+    U_usuario = input("Nombre de jugador incorrecto, vuelva a intentarlo: ")
+contra_usuario = input("Ingrese la contraseña: ")
+while contra_usuario != contraseña:
+    contra_usuario = input("ERROR -- Ingrese nuevamente la contraseña: ")
+validar = validar_contraseña(contraseña,contra_usuario)
+if validar == True:
+    menu()
