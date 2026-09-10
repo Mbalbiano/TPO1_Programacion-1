7f71654e9e8277a3f9a0c5c3a565d0aa1baa5525
Nicolas <alvarengadiegonicolas@gmail.com>
2026-09-10 00:21:26 -0300

    Add introduction function to user menu
    Added an introduction function to the menu flow, enhancing user experience with a narrative context before gameplay.

--- gestion_usuarios/Menu.py
+++ gestion_usuarios/Menu.py
@@ -16,6 +16,7 @@ def menu():
     3. Cerrar sesion""")
         op = int(input(""))
     if op == 1:
+        introduccion()
         completada = juego_ahorcado()
         if completada:
             print("¡Sala 1 completada!")
@@ -28,5 +29,13 @@ def menu():
         menu()
     else:
         print("--Sesion cerrada--")
+def introduccion():
+    print("""Todo empezó con una mala noche de póker. Apostaste más de lo que tenías, firmaste papeles que no leíste con cuidado, y ahora Eduardo —el dueño del casino más temido de la ciudad— dice que le debes una fortuna en plata. Esta noche recibiste una carta sellada con cera negra: "Ven al casino a medianoche. O pagas tu deuda jugando mis juegos... o la pagas de otra forma."
+    No tuviste opción. Cruzaste las puertas del casino "Wachington D.C.", y apenas pisaste la alfombra roja, escuchaste el cerrojo de la entrada cerrarse detrás de ti. Las luces de neón parpadean, y una voz grave resuena desde algún altavoz oculto:
+
+    "Bienvenido, deudor. Esta casa no perdona deudas... las cobra. Tienes dos salas por delante. Supéralas, y tu deuda queda saldada. Falla, y te quedas aquí, jugando para la casa, para siempre."
+
+    Frente a ti hay dos puertas. Ambas llevan a un juego. Ambas son tu única salida.""")
+
 if __name__ == "__main__":
     menu()
