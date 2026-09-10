1e614abfce016de3699d504808d0999501a768b3
Morris01010101010101010101 <idamiano@uade.edu.ar>
2026-09-09 21:58:04 -0300

    Menu

---
+++ gestion_usuarios/Menu.py
@@ -0,0 +1,31 @@
+from contra import *
+from sala1 import juego_ahorcado
+from fn_sala2 import jugar_batalla
+
+def menu():
+    print("Coloque el numero correspondiete a la accion que desea tomar")
+    print("""
+    1. Jugar a la sala_01
+    2. Cambiar la contraseña
+    3. Cerrar sesion""")
+    op = int(input(""))
+    while op !=1 and op !=2 and op !=3:
+        print("""La opcion elegida no es valida, vuelva a intentar
+    1. Jugar a la sala_01
+    2. Cambiar la contraseña
+    3. Cerrar sesion""")
+        op = int(input(""))
+    if op == 1:
+        completada = juego_ahorcado()
+        if completada:
+            print("¡Sala 1 completada!")
+            jugar_batalla()
+        else:
+            print("Volviste al menú sin completar la sala.")
+            menu()
+    elif op ==2:
+        cambiar_contraseña()
+    else:
+        print("--Sesion cerrada--")
+        
+menu()
